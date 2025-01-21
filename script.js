83d357d15fc81d2164b87a10403a1ac7fc6c3b7d
Mikael Gille <mikael.gille@gmail.com>
2025-01-21 20:10:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const backToTopButton = document.getElementById("toTop");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
