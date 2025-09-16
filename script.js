ad5075778e8a0ea098a64678d45cc34e8abc89a7
Mikael Gille <mikael.gille@gmail.com>
2025-09-16 14:21:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Hamburgermeny
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+    hamburger.classList.toggle('toggle');
+});
+
+
+// Tillbaka till toppen
 const backToTopButton = document.getElementById("toTop");
 
 window.onscroll = function () {
